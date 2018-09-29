8c3baa4fef1b7d4155edd0ebc6d1304506f914d5
NorthernMan54 <buyminivan-northern@yahoo.ca>
2018-09-29 10:07:43 -0400

    Minor tuning to improve reliability

--- tools/test_2.sh
+++ tools/test_2.sh
@@ -6,9 +6,10 @@ sleep 1
 echo "Off"
 curl -X PUT http://127.0.01:51826/characteristics --header "Content-Type:Application/json" --header "authorization: 031-45-154" --data "{ \"characteristics\": [{ \"aid\": 2, \"iid\": 14, \"value\": 0}] }"
 sleep 1
-#echo "On -2 "
-#curl -X PUT http://127.0.01:51826/characteristics --header "Content-Type:Application/json" --header "authorization: 031-45-154" --data "{ \"characteristics\": [{ \"aid\": 3, \"iid\": 14, \"value\": 50}] }"
-#sleep 1
-#echo "Off"
-#curl -X PUT http://127.0.01:51826/characteristics --header "Content-Type:Application/json" --header "authorization: 031-45-154" --data "{ \"characteristics\": [{ \"aid\": 3, \"iid\": 14, \"value\": 0}] }"
+echo "On -2 "
+curl -X PUT http://127.0.01:51826/characteristics --header "Content-Type:Application/json" --header "authorization: 031-45-154" --data "{ \"characteristics\": [{ \"aid\": 3, \"iid\": 14, \"value\": 1}] }"
+sleep 1
+echo "Off"
+curl -X PUT http://127.0.01:51826/characteristics --header "Content-Type:Application/json" --header "authorization: 031-45-154" --data "{ \"characteristics\": [{ \"aid\": 3, \"iid\": 14, \"value\": 0}] }"
+sleep 1
 #[{"aid":2,"iid":14,"value":0}]
